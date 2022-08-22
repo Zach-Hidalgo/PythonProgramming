a34746da24f2b7b404f09e20c4c9c4252a3f9d68
Zach-Hidalgo <111439976+Zach-Hidalgo@users.noreply.github.com>
2022-08-22 13:06:41 -0500

    First Python

---
+++ First.py
@@ -0,0 +1,7 @@
+# Programmer : Zach
+# Program : Hello world; following the tradition
+# Version : Python 3.8.2 
+# First Written : 08-22-2022, 8:45:00
+# Last Updated : 08-22-2022, 8:45:00
+
+print("Hello World")
