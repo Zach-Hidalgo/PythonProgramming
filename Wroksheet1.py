2f73d714fb9f9f43217e89e03b823d43a3bd9cb9
Zach-Hidalgo <111439976+Zach-Hidalgo@users.noreply.github.com>
2022-09-02 09:30:59 -0500

    Add files via upload

---
+++ Wroksheet1.py
@@ -0,0 +1,22 @@
+print ("Zach Hidalgo")
+print ("Guymon")
+firstInput = 100
+secondInput = 2
+thirdInput = 4
+fourthInput = 3348
+print ("number of seconds in August is",firstInput*secondInput*thirdInput*fourthInput)
+fifthInput = 12
+sixthInput = 403
+print ("dozens of eggs are",sixthInput/fifthInput)
+print ("remainder of eggs is",sixthInput%fifthInput)
+major = 'Computer Science'
+basketballNumber = '21'
+print (major)
+print (basketballNumber)
+print ("U+0000")
+print ("U+0006")
+print ("U+001A")
+print ("U+003F")
+print ("U+0032")
+print ("U+003D")
+
