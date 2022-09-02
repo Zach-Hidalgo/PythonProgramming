e6fb24e8f2ba94533b0c497c8e3ae3a7ada98351
Zach-Hidalgo <111439976+Zach-Hidalgo@users.noreply.github.com>
2022-09-02 09:31:13 -0500

    Add files via upload

---
+++ Lab2.py
@@ -0,0 +1,9 @@
+print (21, 4*32, "hello")
+x=18
+print (x, bin(x), hex(x))
+y=0b1011
+z=0xA3
+print (y, z)
+w = x + y + z
+print ("the sum is", w)
+#there is a string in line 3 and the float object cannot be interpreted as an integer
