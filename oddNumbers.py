9acec856502c56f4c9896a2084ea31130cb32852
Zach <Zachariah.Hidalgo@sckans.edu>
2022-09-21 08:28:07 -0500

    .\oddNumbers.py

---
+++ oddNumbers.py
@@ -0,0 +1,10 @@
+#Zach
+#oddNumbers
+#9-21-2022 08:04:00
+#9-21-2022 08:04:00
+#3.10.7
+
+print("Printing Odd Numbers from the First Hundred Natural Numbers")
+for i in range(0,101):
+    if (i%2 != 0):
+        print(i,end= " ")
