f149e64b32e5ba63502dbf22da4d22036226f44b
Zach <Zachariah.Hidalgo@sckans.edu>
2022-09-21 08:45:19 -0500

    Added Alternate Aproach

--- oddNumbers.py
+++ oddNumbers.py
@@ -4,7 +4,12 @@
 #9-21-2022 08:04:00
 #3.10.7
 
-print("Printing Odd Numbers from the First Hundred Natural Numbers")
-for i in range(0,101):
-    if (i%2 != 0):
-        print(i,end= " ")
+# print("Printing Odd Numbers from the First Hundred Natural Numbers")
+# for i in range(0,101):
+#     if (i%2 != 0):
+#         print(i,end= " ")
+
+print("Alternate Arrangement")
+for k in range(1,101):
+    print((2*k)-1, end=" ")
+
