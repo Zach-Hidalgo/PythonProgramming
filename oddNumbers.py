6cfddc5529247000676cddad2b886147b43680db
Zach <Zachariah.Hidalgo@sckans.edu>
2022-09-23 09:20:58 -0500

    Added reverse odd numbers

--- oddNumbers.py
+++ oddNumbers.py
@@ -9,7 +9,19 @@
 #     if (i%2 != 0):
 #         print(i,end= " ")
 
-print("Alternate Arrangement")
-for k in range(1,51):
-    print((2*k)-1, end=" ")
+# print("Alternate Arrangement")
+# for k in range(1,51):
+#     print((2*k)-1, end=" ")
+
+print("Printing odd numbers from first 100 natural numbers using while loop")
+i = 100
+while i >= 1:
+    i = i - 1
+    if i % 2 != 0:
+        print(i, end=" ")
+
+
+
+
+
 
