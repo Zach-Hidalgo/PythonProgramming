9163a45ea3be053332ee28c72bb0212b0ee28daf
Zach <Zachariah.Hidalgo@sckans.edu>
2022-10-05 08:24:39 -0500

    .\algorithm.py

---
+++ algorithm.py
@@ -0,0 +1,44 @@
+# n = int(input("What Time is it in Military Time\t"))
+# if n >= 800:
+#     print("Enjoy Working")
+# elif n <= 1700:
+#     print("Enjoy Working")
+# elif n > 1700:
+#     print("Go Home")
+
+# print("Natural Numbers Between 100 and 200")
+# for i in range(100,201):
+#     print(i,end=" ")
+
+# Name = " Zach"
+# print("Hello"+ Name)
+
+print("The area of a circle\n")
+PI = 3.14
+radius = float(input("Please Enter the radius of a circle:\n"))
+area = PI * radius * radius
+print(" Area Of a Circle = %.2f" %area)
+
+print("The  area of a rectangle")
+w = float(input("Enter the width of the rectangle:\t"))
+l = float(input("Enter the length of the rectangle:\t"))
+i = l * w
+print("Area of a rectangle is",i)
+
+print("The area of a triangle")
+b = float(input("Enter the base of the triangle:\t"))
+h = float(input("Enter the height of the triangle:\t"))
+i = (b*h)/2
+print("Area of the Triangle is",i)
+
+print("The area of a Rhombus")
+p = float(input("Enter the first diagonal:\t"))
+q = float(input("Enter the second diagonal:\t"))
+i = (p*q)/2
+print("Area of the Rhombus is",i)
+
+import math
+print("Printing square roots")
+i = float(input("Enter your number:\t"))
+print(math.sqrt(i),)
+
