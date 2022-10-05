f529fb48c71318a397726fb246fcb9cb69fff308
Zach <Zachariah.Hidalgo@sckans.edu>
2022-10-05 08:26:26 -0500

    .\oddNumbers.py

--- oddNumbers.py
+++ oddNumbers.py
@@ -4,21 +4,21 @@
 #9-21-2022 08:04:00
 #3.10.7
 
-# print("Printing Odd Numbers from the First Hundred Natural Numbers")
-# for i in range(0,101):
-#     if (i%2 != 0):
-#         print(i,end= " ")
+print("Printing Odd Numbers from the First 500 Natural Numbers\t")
+for i in range(0,500):
+    if (i%2 != 0):
+        print(i,end= " ")
 
 # print("Alternate Arrangement")
 # for k in range(1,51):
 #     print((2*k)-1, end=" ")
 
-print("Printing odd numbers from first 100 natural numbers using while loop")
-i = 100
-while i >= 1:
-    i = i - 1
-    if i % 2 != 0:
-        print(i, end=" ")
+# print("Printing odd numbers from first 100 natural numbers using while loop")
+# i = 100
+# while i >= 1:
+#     i = i - 1
+#     if i % 2 != 0:
+#         print(i, end=" ")
 
 
 
