48360f35e5bbbd1f1ae285ba9b4eea8894abd0b8
Zach <Zachariah.Hidalgo@sckans.edu>
2022-10-05 08:14:10 -0500

    .\algorithm.py

---
+++ squareRoot.py
@@ -0,0 +1,10 @@
+#Zach
+#squareRoot
+#9/23/2022 09:30:00
+#9/23/2022 09:30:00
+#3.30.7
+import math
+print("Printing square roots of first 100 natural numbers")
+for i in range(1,101):
+    print(math.sqrt(i), end=" ")
+
